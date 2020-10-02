018a14ca525b57db63f356f95f82e34352031176
Chris Reynolds <chris@jazzsequence.com>
2020-10-02 16:31:14 -0600

    define db constants

---
+++ .config/load-early.php
@@ -0,0 +1,7 @@
+<?php
+define( 'DB_NAME', 'wordpress' );
+define( 'DB_USER', 'root' );
+define( 'DB_PASSWORD', 'd0d469444e274531cba5a67ffb8d70df432cad20b26f52ec' );
+define( 'DB_HOST', 'localhost' );
+define( 'DB_CHARSET', 'utf8' );
+define( 'DB_COLLATE', '' );
