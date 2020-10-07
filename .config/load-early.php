c2662e384cb1592ed4e1e548005b7ce68af5a3e7
Chris Reynolds <chris@humanmade.com>
2020-10-07 14:31:11 -0600

    only define db info when we're not local

--- .config/load-early.php
+++ .config/load-early.php
@@ -1,7 +1,9 @@
 <?php
-define( 'DB_NAME', 'wordpress' );
-define( 'DB_USER', 'root' );
-define( 'DB_PASSWORD', 'd0d469444e274531cba5a67ffb8d70df432cad20b26f52ec' );
-define( 'DB_HOST', 'localhost' );
-define( 'DB_CHARSET', 'utf8' );
-define( 'DB_COLLATE', '' );
+if ( ! Altis\get_environment_type() === 'local' ) {
+	define( 'DB_NAME', 'wordpress' );
+	define( 'DB_USER', 'root' );
+	define( 'DB_PASSWORD', 'd0d469444e274531cba5a67ffb8d70df432cad20b26f52ec' );
+	define( 'DB_HOST', 'localhost' );
+	define( 'DB_CHARSET', 'utf8' );
+	define( 'DB_COLLATE', '' );
+}
