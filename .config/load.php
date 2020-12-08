cd986c7e5de1257960d874a6271bedd823bd4ea1
Chris Reynolds <chris@jazzsequence.com>
2020-12-08 11:53:43 -0700

    just use load-early

--- .config/load.php
+++
@@ -1,7 +0,0 @@
-<?php
-define( 'DB_NAME', 'db' );
-define( 'DB_USER', 'db' );
-define( 'DB_PASSWORD', 'dkbbmc73utuu1h8a' );
-define( 'DB_HOST', 'app-e3c2cafb-3e7d-46e6-92cf-552921225a1c-do-user-2522501-0.b.db.ondigitalocean.com:25060' );
-define( 'DB_CHARSET', 'utf8' );
-define( 'DB_COLLATE', '' );
