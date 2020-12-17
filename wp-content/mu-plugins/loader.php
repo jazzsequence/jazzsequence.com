686d2b66a1b57da5a6ec2644fc73bc5d962b009d
Chris Reynolds <chris@humanmade.com>
2020-12-17 12:09:11 -0700

    add browser security namespace to loader we need it for the browser security plugin since we aren't using composer's autoloader.

--- wp-content/mu-plugins/loader.php
+++ wp-content/mu-plugins/loader.php
@@ -14,6 +14,7 @@ if ( ( defined( 'WP_INSTALLING' ) && WP_INSTALLING ) ) {
 $hm_mu_plugins = array(
 	'cmb2/init.php',
 	'altis-cms/plugin.php',
+	WP_PLUGIN_DIR . '/browser-security/inc/namespace.php',
 );
 
 foreach ( $hm_mu_plugins as $file ) {
