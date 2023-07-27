aa2e73c20318262695722c12ce7304cf0fef3732
Chris Reynolds <chris@jazzsequence.com>
2023-07-27 12:41:15 -0600

    create release script maybe it works?

--- bin/create_release.sh
+++ bin/create_release.sh
@@ -20,34 +20,6 @@ function get_pr_details() {
   fi
 }
 
-function get_current_version() {
-  local release_list=$(gh release list)
-  echo "$release_list" | grep -o 'Latest[[:space:]]\+[0-9]\+\.[0-9]\+\.[0-9]\+' | grep -o '[0-9]\+\.[0-9]\+\.[0-9]\+' | head -1
-}
-
-function increment_version() {
-  local pr_title=$1
-  local pr_labels=$2
-
-  local current_version=$(get_current_version)
-  local version=""
-
-  # Attempt to extract version from PR title
-  version=$(echo "$pr_title" | grep -oE '[0-9]+\.[0-9]+\.[0-9]+')
-
-  if [[ -z "$version" ]]; then
-    # If version not found in PR title, try to extract from labels
-    version=$(echo "$pr_labels" | grep -oE '[0-9]+\.[0-9]+\.[0-9]+')
-  fi
-
-  if [[ -z "$version" ]]; then
-    # If version still not found, fallback to current version
-    version=$current_version
-  fi
-
-  echo "$version"
-}
-
 function get_release_title() {
   local pr_body=$1
   local heading=$(echo "$pr_body" | grep -m 1 '^# ')
@@ -55,7 +27,7 @@ function get_release_title() {
   if [[ -n "$heading" ]]; then
     echo "${heading/#\#+([[:space:]])/}"
   else
-    echo "Release $version"
+    echo ""
   fi
 }
 
@@ -72,11 +44,7 @@ function create_release() {
   echo "Release Date: $release_date"
 
   if [[ "$DRY_RUN" == "true" ]]; then
-    # Print the dry run message only once
-    if [[ ! "$dry_run_printed" ]]; then
-      echo "Dry run enabled. Release not created."
-      dry_run_printed=true
-    fi
+    echo "Dry run enabled. Release not created."
   else
     # Uncomment the following line to create the actual release
     # gh release create "v$version" --title "$release_title" --notes "$release_notes"
@@ -111,60 +79,49 @@ merged_prs=$(gh pr list -R jazzsequence/jazzsequence.com -s merged -B main --jso
 # Filter out PRs with release version in title or labels
 filtered_prs=$(echo "$merged_prs" | jq -c 'map(select(.title | test("Release [0-9]+\\.[0-9]+\\.[0-9]+")))')
 
-# Process the filtered PRs using a for loop and jq directly
-dry_run_printed=false
-while IFS= read -r pr_info; do
-  # Decode base64 encoded JSON for each PR
-  decoded_pr_info=$(echo "$pr_info" | base64 --decode)
-
-  # Extract PR details using jq
-  pr_number=$(echo "$decoded_pr_info" | jq -r '.number')
-  pr_title=$(echo "$decoded_pr_info" | jq -r '.title')
-  pr_labels=$(echo "$decoded_pr_info" | jq -r '.labels')
-
-  # Get PR details
-  merged_at=$(get_pr_details "$pr_number" | jq -r '.mergedAt')
-  pr_body=$(get_pr_details "$pr_number" | jq -r '.body')
-
-  # Increment version
-  version=$(increment_version "$pr_title" "$pr_labels")
-
-  # Get release title
-  release_title=$(get_release_title "$pr_body")
-
-  # Format release date (you can customize this as per your preference)
-  release_date=$(date '+%B %d, %Y')
-
-  # Create the release
-  create_release "$version" "$release_title" "$pr_body" "$release_date"
-
-done <<< "$filtered_prs"
-
-if [[ -z "${merged_prs}" ]]; then
+# Check if there are any PRs with version patterns
+if [[ -z "$filtered_prs" ]]; then
+  # No PRs with version patterns found, check if there is a manual version provided
   if [[ -z "${MANUAL_VERSION}" ]]; then
-    # Get the latest release version using the provided script
-    latest_release=$(get_current_version)
-
-    if [[ -z "${latest_release}" ]]; then
-      echo "No merged PR found with a title following the pattern 'Release X.X.X', and no manual version provided."
-      echo "No previous releases found."
-
-      # Exit successfully as there's nothing to release
-      exit 0
-    fi
-
-    version=$(increment_version "" "")
-    release_title="${PR_TITLE_PREFIX} ${version}"
-    release_notes="Release from previous release"
-    release_date=$(date "+%B %d, %Y")
-    create_release "${version}" "${release_title}" "${release_notes}" "${release_date}"
+    echo "Warning: No merged PR found with a title following the pattern 'Release X.X.X', and no manual version provided."
+    echo "No previous releases found."
+    exit 1
   else
-    echo "Warning: No merged PR found with a title following the pattern 'Release X.X.X'. Using manual version provided."
     version="${MANUAL_VERSION}"
     pr_title="Release ${MANUAL_VERSION}"
-    release_title="${PR_TITLE_PREFIX} ${version}"
-    release_notes="Manual release"
-    release_date=$(date "+%B %d, %Y")
-    create_release "${version}" "${release_title}" "${release_notes}" "${release_date}"
+    pr_body=""
   fi
+else
+  # Process the filtered PRs using a for loop and jq directly
+  for pr_info in $(echo "$filtered_prs" | jq -r '.[] | @base64'); do
+    # Decode base64 encoded JSON for each PR
+    decoded_pr_info=$(echo "$pr_info" | base64 --decode)
+
+    # Extract PR details using jq
+    pr_number=$(echo "$decoded_pr_info" | jq -r '.number')
+    pr_title=$(echo "$decoded_pr_info" | jq -r '.title')
+    pr_labels=$(echo "$decoded_pr_info" | jq -r '.labels')
+
+    # Get PR details
+    merged_at=$(get_pr_details "$pr_number" | jq -r '.mergedAt')
+    pr_body=$(get_pr_details "$pr_number" | jq -r '.body')
+
+    # Extract release version from PR title
+    version=$(echo "$pr_title" | grep -oE '[0-9]+\.[0-9]+\.[0-9]+')
+    if [[ -z "$version" ]]; then
+      echo "Warning: Version not found in PR title: $pr_title. Skipping release for PR #$pr_number."
+      continue
+    fi
+
+    break  # Exit loop after processing the first PR with a version pattern
+  done
 fi
+
+# Get release title
+release_title=$(get_release_title "$pr_body")
+
+# Format release date (you can customize this as per your preference)
+release_date=$(date '+%B %d, %Y')
+
+# Create the release
+create_release "$version" "$release_title" "$pr_body" "$release_date"
