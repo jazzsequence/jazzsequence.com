cae7fe57f755c30656a31b7867d03422a09393c7
Chris Reynolds <chris@jazzsequence.com>
2023-08-09 14:51:04 -0600

    don't chmod already done

--- bin/dry-run-release.sh
+++ bin/dry-run-release.sh
@@ -2,8 +2,6 @@
 VERSION=$(jq -r .version ./version.json) 
 # If WORKSPACE_PATH is not set, set it to ./ (the current directory)
 WORKSPACE_PATH=${WORKSPACE_PATH:-./}
-PATH=${PATH:-./}
-chmod +x "$WORKSPACE_PATH"/bin/create_release.sh
 echo \"Releasing "$VERSION"\"
 # Run the script at the WORKSPACE_PATH.
 "$WORKSPACE_PATH"/bin/create_release.sh --version "$VERSION" --dry-run
