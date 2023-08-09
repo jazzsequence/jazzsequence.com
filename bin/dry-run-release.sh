56d98ed943fe991b84d75a344817fef636a2bbb9
Chris Reynolds <chris@jazzsequence.com>
2023-08-09 14:43:29 -0600

    store the workspace path as an environment variable

--- bin/dry-run-release.sh
+++ bin/dry-run-release.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
 VERSION=$(jq -r .version ./version.json) 
+# If WORKSPACE_PATH is not set, set it to ./ (the current directory)
+WORKSPACE_PATH=${WORKSPACE_PATH:-./}
+PATH=${PATH:-./}
 echo \"Releasing "$VERSION"\"
-bin/create_release.sh --version "$VERSION" --dry-run
+# Run the script at the WORKSPACE_PATH.
+"$WORKSPACE_PATH"/bin/create_release.sh --version "$VERSION" --dry-run
