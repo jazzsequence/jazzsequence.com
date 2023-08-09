43eadf509b562411f4b1ae763207be958675d715
Chris Reynolds <chris@jazzsequence.com>
2023-08-09 16:02:06 -0600

    try again

--- bin/create_release.sh
+++ bin/create_release.sh
@@ -28,7 +28,7 @@ function get_release_title() {
   heading=$(echo "$pr_body" | grep -m 1 '^# ')
   
   if [[ -n "$heading" ]]; then
-    echo "${heading/#\#+([[:space:]])/}"
+    echo "${heading}" | sed 's/^# *//'
   else
     echo ""
   fi
