ceb06caaa02d837851caf3d7bc6e4e7afe036de1
Chris Reynolds <chris@jazzsequence.com>
2023-08-09 14:38:57 -0600

    change the path to the script

--- bin/dry-run-release.sh
+++ bin/dry-run-release.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 VERSION=$(jq -r .version ./version.json) 
 echo \"Releasing "$VERSION"\"
-./bin/create_release.sh --version "$VERSION" --dry-run
+bin/create_release.sh --version "$VERSION" --dry-run
