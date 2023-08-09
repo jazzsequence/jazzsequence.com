738168da161278f494af34e82d46dfcf2506a547
Chris Reynolds <chris@jazzsequence.com>
2023-08-09 15:49:39 -0600

    add +x if necessary

--- bin/dry-run-release.sh
+++ bin/dry-run-release.sh
@@ -9,5 +9,12 @@ if [[ ! -f "$WORKSPACE_PATH"/bin/create_release.sh ]]; then
   echo "create_release.sh script not found. Exiting."
   exit 1
 fi
+
+# Check permissions of the create_release script. If it's not set to execute, add +x.
+if [[ ! -x "$WORKSPACE_PATH"/bin/create_release.sh ]]; then
+  echo "create_release.sh script is not executable. Adding +x."
+  chmod +x "$WORKSPACE_PATH"/bin/create_release.sh
+fi
+
 # Run the script at the WORKSPACE_PATH.
 "$WORKSPACE_PATH"/bin/create_release.sh --version "$VERSION" --dry-run
