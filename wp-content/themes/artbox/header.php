74d5fb1225e23b116227a917a6309f1282aa7521
Chris Reynolds <chris@jazzsequence.com>
2025-03-10 09:45:59 -0600

    modernize header and footer remove unnecessary old scripts

--- wp-content/themes/artbox/header.php
+++ wp-content/themes/artbox/header.php
@@ -11,25 +11,11 @@
 	</style>
 
 	<link rel="alternate" type="application/rss+xml" title="RSS 2.0" href="<?php bloginfo('rss2_url'); ?>" />
-	<link rel="alternate" type="text/xml" title="RSS .92" href="<?php bloginfo('rss_url'); ?>" />
 	<link rel="alternate" type="application/atom+xml" title="Atom 1.0" href="<?php bloginfo('atom_url'); ?>" />
 
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>" />
 	<?php wp_get_archives('type=monthly&format=link'); ?>
-	<?php //comments_popup_script(); // off by default ?>
 	<?php wp_head(); ?>
-<!--this makes transparent pngs not suck in ie5-6-->
-<!--[if lt IE 7.]>
-<script defer type="text/javascript" src="pngfix.js"></script>
-<![endif]-->
-<!--this is calling an alternate stylesheet for ie-->
-<!--[if IE]>
-<style type="text/css">
-#content {
-	
-   }
-</style>
-<![endif]-->    
 </head>
 
 <body>
