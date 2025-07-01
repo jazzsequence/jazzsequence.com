923e13f84541480e6e328928f7eb549fc14a0f6c
Chris Reynolds <chris@jazzsequence.com>
2025-06-30 23:28:07 -0600

    Release 1.7.0 (#220)
    * don't need to use sunrise anymore
    
    * remove altis build script
    don't need it
    
    * Add Object Cache Pro (#219)
    
    * ignore auth.json
    
    * create auth.json before composer install
    
    * add ocp
    
    * Ninja forms and Composer updates (#218)
    
    * better architecture for scripts
    and remove the build-script file
    
    * composer update which gets us latest ninja forms
    
    * bump version to 1.7.0

--- wp-config.php
+++ wp-config.php
@@ -17,7 +17,6 @@ define( 'SUBDOMAIN_INSTALL', true );
 define( 'PATH_CURRENT_SITE', '/' );
 define( 'SITE_ID_CURRENT_SITE', 1 );
 define( 'BLOG_ID_CURRENT_SITE', 1 );
-define( 'SUNRISE', 'on' );
 
 /**
  * Define Dashboard Changelog repository.
